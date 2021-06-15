61dcdf82090cec5251f1d9d9654ca9d8d32961d0
StackMeter <83759440+StackMeter@users.noreply.github.com>
2021-06-15 14:54:23 +0100

    slow progress is still progress
    (with emphasis) figured it out

--- interpreter.py
+++ interpreter.py
@@ -9,19 +9,42 @@ for i in range(52):
   else:
     forbiddencharacters.append(chr(i + 65))
 stringmode = listmode = setmode = tuplemode = conditionalmode = functionmode = commentmode = variablemode = numbermode = False
-characterlist1 = ["\n" ", "!", "\"", "#", "$", "%", "\'", "(", ")", "*", "+", ",", "-", ".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "[", "/", "]", "^", "_", "`", "{", "|", "}", "~"] #the legal characters
-# here's the string and the comment section
-currentstring1 = currentitem1 = currentstring2 = currenitem2 = ""
-index = 0
+characterlist1 = 
+"""
+¶ 
+"""
+# Things that will make my life easier
+def findThisList(item, li):
+  return [i for i in range(len(li)) if li[i] == item]
+def findThisString(ch, s):
+  return [i for i, ltr in enumerate(s) if ltr == ch]
+def find(item, string): # This will actually be a helper function for a command called [insert here]
+  li = []
+  try:
+    li = findthisList(item, list(code.split()))
+  except Exception as e:
+    li = findthisString(item, str(code))
+  
 def flagThis(code, characters, flags):
+  li = []
+  for chr in characters:
+    li += find(chr, code)
+  for i1 in li:
+    for i2 in i1:
+      code[i2] = [code[i2], flags[li.index(i1)]]
+  return code
+
+   
+      
+  
   for chr in code:
     if chr in characters:
-      index1 = characters.index(chr)
+      
       
   
 
 def run(thisCode):  
-  for chr in cset:
+  for chr in thisCode:
     if chr in forbiddencharacters and not stringmode and not commentmode:
       print("Error: alphanumeric characters detected outside of a string or comment.")
       sys.exit()
