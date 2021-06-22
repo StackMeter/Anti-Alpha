bb68aa2d103bf9fb88d92ce829edc7c9cae29fe6
StackMeter <83759440+StackMeter@users.noreply.github.com>
2021-06-22 15:24:36 +0100

    Update and rename math.py to basicmath.py

---
+++ basicmath.py
@@ -0,0 +1,19 @@
+import math as m #here comes the arrows 
+
+def squareRoot(n):
+  return n ** (1/2)
+def cubeRoot(n):
+  return n ** (1/3)
+def nthRoot(m, n):
+  return n ** (1/m) #technically only this is needed, but BUILTINS 
+def sin(x): 
+  return m.sin(x)
+def cos(x):
+  return squareRoot(1 - (sin(x) ** 2), squareRoot(1 - (sin(x) ** 2) * -1
+                                                  
+                                         
+def tan(x):
+                                          
+                                          
+ 
+                                                   
